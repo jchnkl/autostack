a02381033e3025be1bba75b9240a655342d1c3f8
Jochen Keil <jochen.keil@gmail.com>
2015-01-11 21:14:10 +0100

    Clean up module, improve

--- Data/AppStack.hs
+++ Data/AppStack.hs
@@ -1,41 +1,15 @@
--- {-# LANGUAGE AllowAmbiguousTypes       #-}
--- {-# LANGUAGE DeriveFunctor       #-}
-{-# LANGUAGE ExistentialQuantification       #-}
--- {-# LANGUAGE FunctionalDependencies       #-}
--- {-# LANGUAGE ImpredicativeTypes       #-}
--- {-# LANGUAGE RecordWildCards       #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
--- {-# LANGUAGE StandaloneDeriving       #-}
--- {-# LANGUAGE TypeFamilies       #-}
-{-# LANGUAGE UndecidableInstances       #-}
-{-# LANGUAGE ConstraintKinds       #-}
-{-# LANGUAGE DeriveDataTypeable       #-}
-{-# LANGUAGE FlexibleContexts       #-}
-{-# LANGUAGE FlexibleInstances       #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving       #-}
-{-# LANGUAGE MultiParamTypeClasses       #-}
-{-# LANGUAGE OverlappingInstances       #-}
-{-# LANGUAGE RankNTypes       #-}
--- {-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE LambdaCase       #-}
-{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE DeriveDataTypeable        #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE RankNTypes                #-}
 
 module Data.AppStack where
 
--- import Control.Applicative
 import Control.Monad.IO.Class (MonadIO)
--- import Control.Monad.Catch -- (bracket)
--- import Control.Monad.Reader
--- import Control.Monad.State
--- import Data.Typeable
+import Data.Typeable (Typeable)
 
-
-
--- import Data.AppStack.TH
-
-data Allocator t = forall r. Allocator
-    { _acquire :: MonadIO m => m r
-    , _release :: MonadIO m => r -> m ()
-    , _run :: MonadIO m => t m a -> r -> m a
+data Component t = forall d. Typeable d => Component
+    { _startup :: MonadIO m => m d
+    , _cleanup :: MonadIO m => d -> m ()
+    , _retract :: MonadIO m => t m a -> r -> m a
     }
+    deriving Typeable
