099fdaac65396cb7c70b78737b61869a2462c21b
Jochen Keil <jochen.keil@gmail.com>
2015-01-11 22:08:57 +0100

    Fix wrong type variable

--- Data/AppStack.hs
+++ Data/AppStack.hs
@@ -10,6 +10,6 @@ import Data.Typeable (Typeable)
 data Component t = forall d. Typeable d => Component
     { _startup :: MonadIO m => m d
     , _cleanup :: MonadIO m => d -> m ()
-    , _retract :: MonadIO m => t m a -> r -> m a
+    , _retract :: MonadIO m => t m a -> d -> m a
     }
     deriving Typeable
